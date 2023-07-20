d7cea7707c47eada481f0f3159e2fcd2d584b6fb
Ashish jha <113693962+Ashjha75@users.noreply.github.com>
2023-07-20 11:48:13 +0530

    Base commit of vite-react with tailwindcss and express

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="bg-slate-200 h-screen">REACT APP WORKING</div>
+    </>
+  );
+}
+
+export default App;
