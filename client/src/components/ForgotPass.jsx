7148049ef6115dee194f877dcb8503cffd6f092d
Ashish jha <113693962+Ashjha75@users.noreply.github.com>
2023-07-28 19:57:52 +0530

    friday commit

--- client/src/components/ForgotPass.jsx
+++ client/src/components/ForgotPass.jsx
@@ -43,13 +43,43 @@ const ForgotPass = () => {
   }
   return (
     <div className="bg-blue-100 h-screen flex items-center">
-      <div className="w-96 mx-auto rounded-xl shadow-lg bg-white ">
+      {/* <div className="w-96 mx-auto rounded-xl shadow-lg bg-white ">
         <div className="p-5">
           <div className="font-semibold text-2xl my-5 text-center">
             Forgot Password
           </div>
           <div className="text-center text-gray-500  my-5">
             Enter your registered email to recover your password
+          </div>
+          <form className="" onSubmit={submitHandler}>
+            <input
+              type="email"
+              placeholder="email"
+              name="email"
+              autoComplete="off"
+              className="block w-full rounded-md p-3  mb-2 border outline-none focus:ring focus:ring-violet-200"
+              onChange={inputHandler}
+              value={formInput.email}
+            />
+
+            <div className="flex gap-3">
+              <button className="text-white block w-full rounded-full p-2 mt-5 bg-gray-500 font-semibold text-lg ">
+                Cancel
+              </button>
+              <button className="text-white block w-full rounded-full p-2 mt-5 bg-blue-600 font-semibold text-lg ">
+                Send Code
+              </button>
+            </div>
+          </form>
+        </div>
+      </div> */}
+      <div className="w-96 mx-auto rounded-xl shadow-lg bg-white ">
+        <div className="p-5">
+          <div className="font-semibold text-2xl my-5 text-center">
+            Forgot Password
+          </div>
+          <div className="text-center text-gray-500  my-5">
+            Enter otp recived at your registered email to recover your password
           </div>
           <form className="" onSubmit={submitHandler}>
             <input
